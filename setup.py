8025917c661ffd6c91c961adaf8447f051117bee
pbron74 <110968666+pbron74@users.noreply.github.com>
2025-11-25 18:22:18 +0100

    Update setup.py to include new packages
    Added additional packages to the setup configuration.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
-APP = ['main_menu.py']  # hoofdscript
-DATA_FILES = [('_internal', ['_internal/*'])]  # bundel interne map
+APP = ['main_menu.py']  # jouw hoofdscript
 OPTIONS = {
     'argv_emulation': True,
-    'iconfile': 'icon.icns',  # optioneel icoon voor macOS
-    'packages': ['tkinter', 'logging']  # voeg gebruikte modules toe
+    'iconfile': 'icon.icns',
+    'packages': [
+        'tkinter',
+        'logging',
+        'clustering',
+        'vallenplan',
+        'scrape_en_exporteer'
+    ]
 }
 
 setup(
     app=APP,
-    name='AHlauncher',  # naam van de app
+    name='AHlauncher',
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
