75ed64a35396208fc9605a101249a8f07060b451
pbron74 <110968666+pbron74@users.noreply.github.com>
2025-11-25 18:06:31 +0100

    Change main script from AHlauncher.py to main_menu.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
-APP = ['AHlauncher.py']  # jouw main script
+APP = ['main_menu.py']  # hoofdscript
 DATA_FILES = [('_internal', ['_internal/*'])]  # bundel interne map
 OPTIONS = {
     'argv_emulation': True,
-    'iconfile': 'icon.icns',  # optioneel
+    'iconfile': 'icon.icns',  # optioneel icoon voor macOS
     'packages': ['tkinter', 'logging']  # voeg gebruikte modules toe
 }
 
 setup(
     app=APP,
+    name='AHlauncher',  # naam van de app
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
