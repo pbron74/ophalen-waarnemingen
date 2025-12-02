29b1d821ce698001d1a6ce43105fd8ffa53916dc
pbron74 <110968666+pbron74@users.noreply.github.com>
2025-12-02 14:46:41 +0100

    Add files via upload

--- scrape_en_exporteer/__init__.py
+++ scrape_en_exporteer/__init__.py
@@ -1,1 +1,10 @@
+from .scraper import scrape_en_exporteer
+import json, os
+from config import DATA_DIR
 
+# Pad naar gemeenten.json
+gemeenten_path = os.path.join(DATA_DIR, "gemeenten.json")
+
+# Inlezen JSON-bestand
+with open(gemeenten_path, encoding="utf-8") as f:
+    gemeente_dict = json.load(f)
