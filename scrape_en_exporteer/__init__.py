fc7e64a61df754c234c46495ca99667aef1d4d59
pbron74 <110968666+pbron74@users.noreply.github.com>
2025-12-10 17:48:47 +0100

    Update __init__.py

--- scrape_en_exporteer/__init__.py
+++ scrape_en_exporteer/__init__.py
@@ -1,10 +1,11 @@
 from .scraper import scrape_en_exporteer
 import json, os
-from config import DATA_DIR
+from config import GEMEENTEN_FILE, DATA_DIR
 
-# Pad naar gemeenten.json
-gemeenten_path = os.path.join(DATA_DIR, "gemeenten.json")
+# Pad naar gemeenten.json (statisch resource-bestand)
+gemeenten_path = GEMEENTEN_FILE
 
 # Inlezen JSON-bestand
 with open(gemeenten_path, encoding="utf-8") as f:
     gemeente_dict = json.load(f)
+
